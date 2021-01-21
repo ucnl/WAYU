df9da0451c4137b9fe9bc8f1d0a4c93e04cccf88
Aleksandr <aleksandr.v.dikarev@gmail.com>
2021-01-21 15:56:18 +0300

    Create AssemblyInfo.cs

---
+++ WAYU/Properties/AssemblyInfo.cs
@@ -0,0 +1,51 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("WAYU")]
+[assembly: AssemblyDescription("Where Are You Underwater - underwater tracking system host application.\n\n" +
+                               "Latest release: https://api.github.com/repos/ucnl/WAYU/zipball \n" +
+                               "Support: mailto:support@unavlab.com\n\n" +
+                               "More information about WAYU\n" +
+                               "English: https://docs.unavlab.com/navigation_and_tracking_systems_en.html#wayu\n" +
+                               "Russian: https://docs.unavlab.com/navigation_and_tracking_systems_ru.html#wayu\n\n" +
+                               "Project source code:\n" +
+                               "https://github.com/ucnl/WAYU\n\n" +
+                               "This application is 100% open-source, and based on our projects:\n" +
+                               "https://github.com/ucnl/UCNLNav\n" +
+                               "https://github.com/ucnl/UCNLPhysics\n" +
+                               "https://github.com/ucnl/UCNLNMEA\n" +
+                               "https://github.com/ucnl/UCNLKML\n" +
+                               "https://github.com/ucnl/UCNLDrivers\n" +
+                               "https://github.com/ucnl/UCNLUI"
+                               )]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Underwater communication & navigation laboratory, LLC")]
+[assembly: AssemblyProduct("WAYU")]
+[assembly: AssemblyCopyright("Copyright © Underwater communication & navigation laboratory, LLC 2020-2021")]
+[assembly: AssemblyTrademark("UC&NL")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("aec0ef25-4d9f-45fc-943a-eaecc4d081b1")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
