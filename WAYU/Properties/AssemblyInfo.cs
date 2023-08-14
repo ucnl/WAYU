c52b40bcd371e00892715cb130a3d18d6658ca09
Aleksandr <aleksandr.v.dikarev@gmail.com>
2023-08-14 14:11:05 +0300

    Add files via upload

--- WAYU/Properties/AssemblyInfo.cs
+++ WAYU/Properties/AssemblyInfo.cs
@@ -2,9 +2,9 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
 [assembly: AssemblyTitle("WAYU")]
 [assembly: AssemblyDescription("Where Are You Underwater - underwater tracking system host application.\n\n" +
                                "Latest release: https://api.github.com/repos/ucnl/WAYU/zipball \n" +
@@ -21,32 +21,48 @@ using System.Runtime.InteropServices;
                                "https://github.com/ucnl/UCNLKML\n" +
                                "https://github.com/ucnl/UCNLDrivers\n" +
                                "https://github.com/ucnl/UCNLUI\n" +
-                               "https://github.com/ucnl/uOSM"
+                               "https://github.com/ucnl/uOSM\n\n" +
+                               "Hot keys:\n" +
+                               "Ctrl + L: Open/Close connection\n" +
+                               "Ctrl + A: Show all tracks/WAYU pinger only\n" +
+                               "Ctrl + M: Mark current location\n" +
+                               "Ctrl + P: Save a screenshot\n" +
+                               "Ctrl + S: Save tracks as...\n" +
+                               "Ctrl + H: Open current log file\n" +
+                               "Ctrl + O: Open settings editor\n" +
+                               "Ctrl + T: Enable/Disable map tiles\n" +
+                               "Ctrl + +: Zoom In\n" +
+                               "Ctrl + -: Zoom Out\n" +
+                               "Ctrl + F: Enable/Disable target following mode\n" +                               
+                               "Ctrl + 1: Set Base 1 as reference point\n" +
+                               "Ctrl + 2: Set Base 2 as reference point\n" +
+                               "Ctrl + 3: Set Base 3 as reference point\n" +
+                               "Ctrl + 4: Set Base 4 as reference point\n"
                                )]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Underwater communication & navigation laboratory, LLC")]
 [assembly: AssemblyProduct("WAYU")]
-[assembly: AssemblyCopyright("Copyright © Underwater communication & navigation laboratory, LLC 2020-2021")]
+[assembly: AssemblyCopyright("Copyright © Underwater communication & navigation laboratory, LLC 2020-2023")]
 [assembly: AssemblyTrademark("UC&NL")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("aec0ef25-4d9f-45fc-943a-eaecc4d081b1")]
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("8b3e5380-0bd6-4222-b999-f337635216c9")]
 
-// Version information for an assembly consists of the following four values:
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
 //
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.1.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
